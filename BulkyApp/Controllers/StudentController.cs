3306461ea12160e9d780a9de66f55ab3000be416
SawonAnis <anissawon44@gmail.com>
2024-11-07 19:54:36 +0600

    Add project files.

---
+++ BulkyApp/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using BulkyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BulkyApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //ViewBag.Name = Request["Name"];
+            //ViewBag.Id = Request["Id"];
+            return View(new Student());
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(s);
+        }
+        /*[HttpPost]
+        public ActionResult Create(FormCollection fc) {
+            ViewBag.Name = fc["Name"];
+            ViewBag.Id = fc["Id"];
+            return View();
+        }*/
+        /*[HttpPost]
+        public ActionResult Create(string Name,int ID) {
+            ViewBag.Name = Name;
+            ViewBag.Id = ID;
+            return View();
+        }*/
+    }
+}
---
+++ BulkyApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BulkyApp.MvcApplication" Language="C#" %>
