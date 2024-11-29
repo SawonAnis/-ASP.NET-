20c92649024191ab0254a866f87f8f2b93eb19e6
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-11-29 18:59:00 +0600

    Auth

---
+++ Auth/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }  
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Auth/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Authorization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authorization.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(Table_1 u,string ReturnUrl)
+        {
+            return View();
+        }
+
+    }
+}
