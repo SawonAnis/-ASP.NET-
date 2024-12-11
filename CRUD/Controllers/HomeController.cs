9011a8e019027f6eaeed8a34975213665dcaf4eb
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-12-11 19:54:05 +0600

    Add files via upload

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sawon.Models;
+
+namespace Sawon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BulkyContext _context;
+
+        public HomeController(BulkyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Home
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Table1s.ToListAsync());
+        }
+
+        // GET: Home/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table1 = await _context.Table1s
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (table1 == null)
+            {
+                return NotFound();
+            }
+
+            return View(table1);
+        }
+
+        // GET: Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,DisplayOrder")] Table1 table1)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(table1);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(table1);
+        }
+
+        // GET: Home/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table1 = await _context.Table1s.FindAsync(id);
+            if (table1 == null)
+            {
+                return NotFound();
+            }
+            return View(table1);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DisplayOrder")] Table1 table1)
+        {
+            if (id != table1.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(table1);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Table1Exists(table1.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(table1);
+        }
+
+        // GET: Home/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table1 = await _context.Table1s
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (table1 == null)
+            {
+                return NotFound();
+            }
+
+            return View(table1);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var table1 = await _context.Table1s.FindAsync(id);
+            if (table1 != null)
+            {
+                _context.Table1s.Remove(table1);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Table1Exists(int id)
+        {
+            return _context.Table1s.Any(e => e.Id == id);
+        }
+    }
+}
