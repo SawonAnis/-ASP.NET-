dddf9bd153ddbeae6229745143ae13f575c6a0ce
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-12-11 19:53:20 +0600

    Add files via upload

---
+++ CRUD/Models/Table1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sawon.Models;
+
+public partial class Table1
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int DisplayOrder { get; set; }
+}
