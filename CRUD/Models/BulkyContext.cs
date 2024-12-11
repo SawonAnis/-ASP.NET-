b75cb2a73d79d5e4faff58272228f655f9fef187
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-12-11 19:53:06 +0600

    Add files via upload

---
+++ CRUD/Models/BulkyContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sawon.Models;
+
+public partial class BulkyContext : DbContext
+{
+    public BulkyContext()
+    {
+    }
+
+    public BulkyContext(DbContextOptions<BulkyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Table1> Table1s { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Table1>(entity =>
+        {
+            entity.ToTable("Table_1");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
